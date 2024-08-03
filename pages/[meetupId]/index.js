87d90d78c25fc774a36ab255ecba0bd3a198ba5e
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-08-04 02:05:57 +0530

    develop meetup details page with dynamic URL and SSG

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -5,3 +5,49 @@ export default function MeetupDetailsPage() {
         <MeetupDetails />
     )
 }
+
+export const getStaticProps = async(context) => {
+    const meetupId = context.params.meetupId
+
+    console.log(meetupId)
+
+    return{
+        props: {
+            meetupData: {
+                id: "",
+                title: "",
+                image: "",
+                address: "",
+                description: ""
+            }
+        }
+    }
+}
+
+export const getStaticPaths = async() => {
+    return{
+        paths: [
+            // one version of object that URL might be
+            {
+                params: {
+                    meetupId: "M1"
+                }
+            }
+        ],
+        fallback: "blocking",
+    }
+}
+
+
+/**
+ * note
+ * 
+ * Dynamic Routes with SSG
+ * 
+ * - if a page has dynamic routes and uses getStaticProps(),
+ *   it needs to define a list of paths to be statically generated at build time
+ * 
+ * getStaticPaths(), specifies which dynamic routes should be pre-rendered at build time
+ * 
+ * fallback: true | false | "blocking"
+ */
