34cb8bb137d001af09f00b27b2594d948674eb3b
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-08-04 02:41:54 +0530

    develop API routes to send the data to MongoDB

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,23 @@
+import { useRouter } from "next/router"
+
 import NewMeetupForm from "../../components/meetup/NewMeetupForm"
 
 export default function NewMeetupPage() {
-    const addMeetupHandler = (enteredMeetupData) => {}
+    const router = useRouter()
+    
+    const addMeetupHandler = async(enteredMeetupData) => {
+        const response = await fetch("/api/new-meetup", {
+            method: "POST",
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        const enteredData = await response.json()
+        console.log(enteredData)
+        router.push("/")
+    }
 
     return(
         <NewMeetupForm onAddMeetup={addMeetupHandler} />
